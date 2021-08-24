cec3a7bf13ed385d5cc4aaedc75a74699930471b
Ryan Murphy <murp8869@stthomas.edu>
2021-08-24 16:06:20 -0500

    testing commit

---
+++ R/fbind.R
@@ -0,0 +1,4 @@
+fbind <- function(a, b){
+  forcats::fct_c(a, b)
+}
+
---
+++ R/package.R
@@ -0,0 +1,12 @@
+library(usethis)
+
+use_github()
+use_mit_license()
+use_roxygen_md()
+use_r("fbind")
+use_package("forcats")
+use_testthat()
+use_test("fbind")
+use_readme_rmd()
+use_pkgdown()
+
